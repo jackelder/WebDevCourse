673fdfecca03c855b8daa519dce0c80f79d7a07f
Jack Elder <jackelder18@gmail.com>
2019-10-05 17:21:08 -0500

    removed practice

--- practice/forLoops/forLoopsPractice.js
+++
@@ -1,7 +0,0 @@
-/*JavaScript for the for Loops Practice */
-
-var sum=0;
-for(var n=25; n>0; n--){
-  sum=sum+n;
-  console.log(sum);
-}
--- practice/loggingLetters/loggingLetters.js
+++
@@ -1,9 +0,0 @@
-// JavaScript for the Logging Letters Code-Along
-
-var sentence ="I love learning software development.";
-
-for (var i=0; i<sentence.length; i++){
-  if(sentence[i] !==" "){
-    console.log(sentence[i]);
-  }
-}
